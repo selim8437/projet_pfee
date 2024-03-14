2f2aabd41d842a1b25c6f88f4b9a8feacc1173c7
selim8437 <selim.smaali654@gmail.com>
2024-03-14 10:46:43 +0100

    aa

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,9 +1,7 @@
-/*import Pagination from '@/app/ui/invoices/pagination';
-import { fetchInvoicesPages } from '@/app/lib/data';
+import Pagination from '@/app/ui/invoices/pagination';
 import { UserButton } from "@clerk/nextjs";
 
 import Search from '@/app/ui/search';
-import Table from '@/app/ui/invoices/table';
 import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
@@ -19,7 +17,6 @@ export default async function Page({
 }) {
   const query = searchParams?.query || '';
   const currentPage = Number(searchParams?.page) || 1;
-  const totalPages = await fetchInvoicesPages(query);
 
     return (
     <div className="w-full">
@@ -32,11 +29,9 @@ export default async function Page({
         <CreateInvoice />
       </div>
       { <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
-        <Table query={query} currentPage={currentPage} />
       </Suspense> }
       <div className="mt-5 flex w-full justify-center">
-        {  <Pagination totalPages={totalPages} /> }
       </div>
     </div>
   );
-}*/
+}
