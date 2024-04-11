01a39568895b7ee727172c47e93db690a9c8e645
selim8437 <selim.smaali654@gmail.com>
2024-04-11 14:33:08 +0100

    pp

--- app/ui/bankInfo.tsx
+++ app/ui/bankInfo.tsx
@@ -1,5 +1,5 @@
 "use client" ;
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 export default function bankInfo({data}:any){
   const [formData, setFormData] = useState({
