6b5db2733bdb305fb83dbf5eaf0a10dcf27bab00
selim8437 <selim.smaali654@gmail.com>
2024-04-11 14:56:28 +0100

    fixign last warning

--- app/signup/[[...signup]]/page.tsx
+++ app/signup/[[...signup]]/page.tsx
@@ -1,16 +1,23 @@
-"use client" ;
 import RolePage from "@/app/ui/rolee";
-import { SignUp, useClerk, useSession } from "@clerk/nextjs";
-
-import {useRouter} from "next/navigation";
+import { SignUp } from "@clerk/nextjs";
+import { useRouter } from "next/router"; // Change the import here
 import { useEffect, useState } from "react";
 
 export default function Signup() {
+  const router = useRouter();
   const [showSignUp, setShowSignUp] = useState(false); // State to determine whether to show SignUp component
   const [selectedOption, setSelectedOption] = useState<string>(''); // State to store selected option
 
+  useEffect(() => {
+    // Check if the current path is /signup
+    if (router.pathname === '/signup') {
+      setShowSignUp(false); // Show RolePage if the path is /signup
+    } else {
+      setShowSignUp(true); // Show SignUp component for other paths
+    }
+  }, [router.pathname]);
  
-  const handleDataFromChild = (data1:boolean, data2:string) => {
+  const handleDataFromChild = (data1: boolean, data2: string) => {
     console.log('Data received from child:', data1, data2);
     // Update selected option
     setSelectedOption(data2);
