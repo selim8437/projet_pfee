c677acaf24f5c1e1b0b579c8ca41290d51c1463a
selim8437 <selim.smaali654@gmail.com>
2024-04-13 17:21:15 +0100

    selected option

--- app/signup/[[...signup]]/page.tsx
+++ app/signup/[[...signup]]/page.tsx
@@ -1,9 +1,8 @@
-"use client";
-
 import RolePage from "@/app/ui/rolee";
 import { SignUp } from "@clerk/nextjs";
 import { usePathname } from "next/navigation"; // Import usePathname from next/navigation
 import { useEffect, useState } from "react";
+import axios from 'axios'; // Import Axios
 
 export default function Signup() {
   const pathname = usePathname(); // Get the current pathname using usePathname
@@ -11,23 +10,33 @@ export default function Signup() {
   const [selectedOption, setSelectedOption] = useState<string>(''); // State to store selected option
 
   useEffect(() => {
-    // Check if the dfdcurrent path is /signup
-    if (pathname === '/signup/') {
-      setShowSignUp(false); // Show RolePage if the path is /signup
-    } else {
-      setShowSignUp(true); // Show SignUp component for other paths
-    }
+    // Check if the current path is /signup
+    setShowSignUp(pathname !== '/signup/');
   }, [pathname]);
- 
+
+  // Function to handle changes in selected option
   const handleDataFromChild = (data1: boolean, data2: string) => {
     console.log('Data received from child:', data1, data2);
     // Update selected option
     setSelectedOption(data2);
-    console.log(data2)
-    // Show SignUp component
-    setShowSignUp(true);
+    console.log(data2);
+
+    // Send selected option to the API
+    sendSelectedOption(data2);
   };
- 
+
+  // Function to send the selected option to the API
+  const sendSelectedOption = async (selectedOption: string) => {
+    try {
+      // Make a POST request to your API endpoint with the selected option
+      await axios.post('/your-api-endpoint', { selectedOption });
+      console.log('Selected option sent to API:', selectedOption);
+    } catch (error) {
+      console.error('Error sending selected option to API:', error);
+      // Handle error
+    }
+  };
+
   return (
     <>
       {showSignUp ? (
