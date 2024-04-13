4f04ce5327d48409ee19ec342b0c89fb57380b65
selim8437 <selim.smaali654@gmail.com>
2024-04-13 18:29:01 +0100

    dsdsd

--- app/signup/[[...signup]]/page.tsx
+++ app/signup/[[...signup]]/page.tsx
@@ -1,24 +1,41 @@
-"use client";
-
+"use client" ;
 import RolePage from "@/app/ui/rolee";
 import { SignUp } from "@clerk/nextjs";
 import { usePathname } from "next/navigation"; // Import usePathname from next/navigation
 import { useEffect, useState } from "react";
-
+import axios from 'axios'; // Import Axios
+interface UserData {
+  id: string;
+  email: string;
+  first_name: string;
+  last_name: string;
+}
 export default function Signup() {
   const pathname = usePathname(); // Get the current pathname using usePathname
   const [showSignUp, setShowSignUp] = useState(false); // State to determine whether to show SignUp component
   const [selectedOption, setSelectedOption] = useState<string>(''); // State to store selected option
+  const [userData, setUserData] = useState<UserData | null>(null);
 
   useEffect(() => {
-    // Check if the dfdcurrent path is /signup
-    if (pathname === '/signup/') {
-      setShowSignUp(false); // Show RolePage if the path is /signup
-    } else {
-      setShowSignUp(true); // Show SignUp component for other paths
-    }
+    // Check if the current path is /signup
+    setShowSignUp(pathname !== '/signup/');
+    
+    // Fetch user data when the component mounts
+    fetchUserData();
   }, [pathname]);
- 
+
+  // Function to fetch user data
+  const fetchUserData = async () => {
+    try {
+      // Make a GET request to fetch user data from the API
+      const response = await axios.get('/api/userData'); // Update the endpoint as per your API
+      const userData = response.data;
+      setUserData(userData);
+    } catch (error) {
+      console.error('Error fetching user data:', error);
+    }
+  };
+
   const handleDataFromChild = (data1: boolean, data2: string) => {
     console.log('Data received from child:', data1, data2);
     // Update selected option
@@ -44,7 +61,17 @@ export default function Signup() {
           </div>
         </div>
       ) : (
-        <RolePage sendDataToParent={handleDataFromChild} />
+        <div>
+          <RolePage sendDataToParent={handleDataFromChild} />
+          {userData && (
+            <div>
+              <p>User ID: {userData.id}</p>
+              <p>Email: {userData.email}</p>
+              <p>First Name: {userData.first_name}</p>
+              <p>Last Name: {userData.last_name}</p>
+            </div>
+          )}
+        </div>
       )}
     </>
   );
