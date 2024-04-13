f86030bca2bd0d3d38cb2edeae4f713fa09c83b5
selim8437 <selim.smaali654@gmail.com>
2024-04-13 17:17:17 +0100

    addddata

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -12,7 +12,7 @@ export async function POST(req: Request) {
   }
 
   // Get the headers
-  const headerPayload = headers(req);
+  const headerPayload = headers();
   const svix_id = headerPayload.get('svix-id');
   const svix_timestamp = headerPayload.get('svix-timestamp');
   const svix_signature = headerPayload.get('svix-signature');
