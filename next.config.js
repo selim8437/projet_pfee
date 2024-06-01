12fe541e9ff3476cffc5bc10a425b885a1005808
selim8437 <selim.smaali654@gmail.com>
2024-06-01 13:45:45 +0100

    correct error 9 deleting page.tsx

--- next.config.js
+++ next.config.js
@@ -1,14 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     trailingSlash: true,
-    images:{
-        domains: ['img.icons8.com'],
-        remotePatterns:[
-            {
-                hostname:'utfs.io'
-            }
-        ]
+    images: {
+      domains: ['img.icons8.com'],
+      remotePatterns: [
+        {
+          hostname: 'utfs.io'
+        }
+      ]
+    },
+    async redirects() {
+      return [
+        {
+          source: '/',
+          destination: '/shop/', // Replace with your desired entry page
+          permanent: true,
+        },
+      ];
     }
-};
-
-module.exports = nextConfig;
+  };
+  
+  module.exports = nextConfig;
+  
