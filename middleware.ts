4c438b5edc77fe4421a5b58553000686b56c7d5b
selim8437 <selim.smaali654@gmail.com>
2024-06-02 14:01:24 +0100

    middleware update

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { authMiddleware } from "@clerk/nextjs";
  
 export default authMiddleware({
   // Routes that can be accessed while signed out
-  publicRoutes: ['/api/webhooks(.*)','/store','/api/image/','/signup(.*)','/signin','/api/uploadthing(.*)',"/api/user"],
+  publicRoutes: ['/api/webhooks(.*)','/store','/api/image/','/signup(.*)','/signin','/api/uploadthing(.*)',"/shop/"],
   // Routes that can always be accessed, and have
   // no authentication information
   ignoredRoutes: ['/'],
