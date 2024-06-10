224560e14ea46333a3619587667b64f5edf89d38
selim8437 <selim.smaali654@gmail.com>
2024-06-10 19:44:06 +0100

    dahboardcolorfix

--- app/dashboard/content/layout.tsx
+++ app/dashboard/content/layout.tsx
@@ -6,7 +6,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         <div  >
             <DashboardLayoutHeader />
         </div>
-        <div className="flex  flex-1 bg-gray-850 flex-col gap-4 p-4 md:gap-8 md:p-6">
+        <div className="flex  flex-1 bg-gray-900 flex-col gap-4 p-4 md:gap-8 md:p-6">
             
             {children}</div>
       </div>
