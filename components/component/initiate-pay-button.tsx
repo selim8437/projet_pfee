e18eaed77d34facec53560db717c1e8d9279f10e
selim8437 <selim.smaali654@gmail.com>
2024-06-10 23:29:19 +0100

    fixxx

--- components/component/initiate-pay-button.tsx
+++ components/component/initiate-pay-button.tsx
@@ -8,6 +8,8 @@ import { getUserByid } from '@/app/lib/users';
 import { User } from '@/app/lib/types/user';
 import { useUser } from '@clerk/nextjs';
 import { CheckIcon } from './consult-order';
+import { getOrderById } from '@/app/lib/orders';
+import { Order } from '@/app/lib/types/order';
 
 const InitPay = ({orderId}:{orderId:string} ) => {
     const [paymentUrl, setPaymentUrl] = useState<string | null>(null);
@@ -15,6 +17,7 @@ const InitPay = ({orderId}:{orderId:string} ) => {
     const [loading, setLoading] = useState<boolean>(false);
     const [error, setError] = useState<string | null>(null);
     const [Id,setUserid]=useState<string >('') ;
+    const [order,setOrder]=useState<Order|null>(null)
   
     
     
@@ -24,11 +27,11 @@ const InitPay = ({orderId}:{orderId:string} ) => {
       setError(null);
       
       try {
-        
+        if(order!=null){
         const initiatePaymentRequest: InitiatePaymentRequest = {
           receiverWalletId: '6649b362a58e585682a5ba1a', // Update with your receiver wallet ID
           token: 'TND',
-          amount: 1000,
+          amount: order.totalPrice,
           type: 'immediate',
           description: 'Payment description',
           acceptedPaymentMethods: ['wallet', 'bank_card', 'e-DINAR','flouci'],
@@ -46,21 +49,38 @@ const InitPay = ({orderId}:{orderId:string} ) => {
           failUrl: 'https://projet-pfee.vercel.app/shop/failure/',
           theme: 'light',
         };
+      
         
         const paymentUrl = await initiatePayment(initiatePaymentRequest);
         setPaymentUrl(paymentUrl);
         const payU=await paymentUrl ;
         console.log(payU);
         redir(payU);
-        
+      }
       } catch (error:any) {
         console.error('Error initiating payment:', error);
         setError(error.message || 'An error occurred while initiating payment');
       }finally {
         setLoading(false);
       }
+    
 };
-  
+const fetchOrder = async () => {
+  try {
+    const fetchedOrder = await getOrderById(orderId);
+
+    if (fetchedOrder) {
+      
+      setOrder(fetchedOrder);
+    }
+  } catch (error) {
+    console.error("Error fetching products:", error);
+  } 
+
+};
+useEffect(()=>{
+  fetchOrder() ;
+},[])
   return (
     <div>
       
